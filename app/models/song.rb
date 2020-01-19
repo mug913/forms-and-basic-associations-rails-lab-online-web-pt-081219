5cea24dba10a81798206e7129d2ce4c004e338df
mug913 <brad.alexander913@gmail.com>
2020-01-19 14:08:03 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,19 @@
 class Song < ActiveRecord::Base
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
   # add associations here
+
+  
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
+
+  def artist_name
+    self.artist ? self.artist.name : nil 
+  end 
+ 
+  
+  accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
 end
